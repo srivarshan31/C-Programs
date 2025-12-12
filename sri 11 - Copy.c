96ef0871b84c126a9757c95807e5eb86bfdb9bfb
srivarshan31 <varshansri.venkat@gmail.com>
2025-12-12 15:24:04 +0530

    Add files via upload

---
+++ sri 11 - Copy.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int a;
+	printf("enter a number:");
+	scanf("%d",&a);
+	if(a>0){
+		printf("the number is positive");	
+	}
+	else{
+		printf("The number is negative");
+	}
+	return 0;
+}
