e520488f4c7a271e2f8a87d6aaaeff1a9c3e47b1
srivarshan31 <varshansri.venkat@gmail.com>
2025-12-12 16:06:40 +0530

    Add files via upload

---
+++ Data Types.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int i= 10;
+printf("%\n",i);
+
+char a = 'b';
+printf("%c\n",a);
+
+char c = 'b';
+printf("%d\n",c);
+
+return 0;
+}
+
---
+++ Print name.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+		printf("%c\n Name : Hannesh S,\n Department = EEE, \n College = SIT College");
+	return 0;
+}
